aa1021c8697b929d998ba74c6d21722083fd8372
Miguel Ángel Betances Beato <miguel_betances1@ucne.edu.do>
2024-03-04 19:17:15 -0400

    Creando Vehiculo detalle

---
+++ Parcial2_AP1_MiguelBetances.Shared/Models/VehiuloDetalle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_AP1_MiguelBetances.Shared.Models
+{
+    public class VehiuloDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+
+
+        [Required(ErrorMessage = "Debes de insertar el Id del vehiculo")]
+        public int VehiculosId { get; set; }
+
+
+        [Required(ErrorMessage = "Debes de insertar el Id del accesorio")]
+        public int AccesorrioId { get; set; }
+
+
+        [Required(ErrorMessage = "Debes de insertar un valor")]
+        public decimal Valor { get; set; }
+    }
+}
