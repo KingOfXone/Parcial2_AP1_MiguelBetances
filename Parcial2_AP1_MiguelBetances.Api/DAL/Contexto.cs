f1d9cc555237f4b20aeb56d067e2587c38f87844
Miguel Ángel Betances Beato <miguel_betances1@ucne.edu.do>
2024-03-04 19:47:36 -0400

    Realizando El Contexto

--- Parcial2_AP1_MiguelBetances.Api/DAL/Contexto.cs
+++ Parcial2_AP1_MiguelBetances.Api/DAL/Contexto.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Parcial2_AP1_MiguelBetances.Shared.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Parcial2_AP1_MiguelBetances.Api.DAL
 {
     public class Contexto: DbContext
     {
+        public DbSet<Vehiculos> Vehiculos { get; set; }
+        public DbSet<Accesorios> Accesorios { get; set; }
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
     }
 }
