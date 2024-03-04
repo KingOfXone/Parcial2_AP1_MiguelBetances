ef343935809241aa6073fc478a95dca7baada700
Miguel Ángel Betances Beato <miguel_betances1@ucne.edu.do>
2024-03-04 19:47:00 -0400

    Realizando clase Accesorios

---
+++ Parcial2_AP1_MiguelBetances.Shared/Models/Accesorios.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_AP1_MiguelBetances.Shared.Models
+{
+    public class Accesorios
+    {
+        [Key]
+        public int AccesorioId { get; set; }
+
+        [Required(ErrorMessage = "Descripción Requerida")]
+
+        public string? Descripción { get; set; }
+    }
+}
