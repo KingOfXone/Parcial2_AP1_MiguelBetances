cadb542074c7ad6f8f58b55d6806270532056183
Miguel Ángel Betances Beato <miguel_betances1@ucne.edu.do>
2024-03-04 19:52:34 -0400

    Modifiando El Contexto

--- Parcial2_AP1_MiguelBetances.Api/DAL/Contexto.cs
+++ Parcial2_AP1_MiguelBetances.Api/DAL/Contexto.cs
@@ -8,5 +8,17 @@ namespace Parcial2_AP1_MiguelBetances.Api.DAL
         public DbSet<Vehiculos> Vehiculos { get; set; }
         public DbSet<Accesorios> Accesorios { get; set; }
         public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Accesorios>().HasData(new List<Accesorios>()
+    {
+            new Accesorios() { AccesorioId = 1, Descripción = "Camara Trasera"},
+            new Accesorios() { AccesorioId = 2, Descripción = "Pantalla Interior"},
+            new Accesorios() { AccesorioId = 3, Descripción = "Interior en Piel" },
+            new Accesorios() { AccesorioId = 4, Descripción = "Sun Roof" },
+            new Accesorios() { AccesorioId = 5, Descripción = "Aros de Lujo"},
+    });
+        }
     }
 }
