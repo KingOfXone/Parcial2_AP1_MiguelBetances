60348870e293f3aee263c1c260752df1b71110a9
Miguel Ángel Betances Beato <miguel_betances1@ucne.edu.do>
2024-03-04 20:09:37 -0400

    Realice la migracion y la base de datos

---
+++ Parcial2_AP1_MiguelBetances.Api/Migrations/20240305000537_Inicial.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Parcial2_AP1_MiguelBetances.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accesorios",
+                columns: table => new
+                {
+                    AccesorioId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripción = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accesorios", x => x.AccesorioId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehiculos",
+                columns: table => new
+                {
+                    VehiculoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Descripción = table.Column<string>(type: "TEXT", nullable: false),
+                    Costo = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Gastos = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehiculos", x => x.VehiculoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VehiculosDetalle",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    VehiculosId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AccesorrioId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Valor = table.Column<decimal>(type: "TEXT", nullable: false),
+                    VehiculoId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehiculosDetalle", x => x.DetalleId);
+                    table.ForeignKey(
+                        name: "FK_VehiculosDetalle_Vehiculos_VehiculoId",
+                        column: x => x.VehiculoId,
+                        principalTable: "Vehiculos",
+                        principalColumn: "VehiculoId");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Accesorios",
+                columns: new[] { "AccesorioId", "Descripción" },
+                values: new object[,]
+                {
+                    { 1, "Camara Trasera" },
+                    { 2, "Pantalla Interior" },
+                    { 3, "Interior en Piel" },
+                    { 4, "Sun Roof" },
+                    { 5, "Aros de Lujo" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehiculosDetalle_VehiculoId",
+                table: "VehiculosDetalle",
+                column: "VehiculoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accesorios");
+
+            migrationBuilder.DropTable(
+                name: "VehiculosDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Vehiculos");
+        }
+    }
+}
